7e9041f44605635f4a801f837f9cd785510df6aa
mayur-nandu <nandumayur95@gmail.com>
2020-01-25 14:53:49 +0530

    initial setup

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+// import {Sidenav} from 'materialize-css';
+import * as M from 'materialize-css';
+
+import {baseurl} from '../../../shared/BaseURL'
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent implements OnInit {
+  baseurl=baseurl+'1g2IYW6i4Ko6ad-d-ohzFInIdADdChgH7';
+  constructor() { 
+  }
+
+
+  ngAfterViewInit(): void {
+    M.Sidenav.init(document.querySelector('.sidenav'));
+  }
+
+  ngOnInit() {
+    
+  }
+
+}
+
