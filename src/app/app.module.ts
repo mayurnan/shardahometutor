be2f32c6218499733aa4f5f7c8444eec88fa9ccc
mayur-nandu <nandumayur95@gmail.com>
2020-01-25 14:53:49 +0530

    initial setup

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AboutusComponent } from './components/aboutus/aboutus.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutusComponent,
+    RegistrationComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/shared/BaseURL.ts
@@ -0,0 +1,2 @@
+
+export const  baseurl='https://drive.google.com/uc?export=view&id='
