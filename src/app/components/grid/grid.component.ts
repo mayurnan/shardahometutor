112061e71858c6363deb5d9a86790336831e8dd3
mayur-nandu <nandumayur95@gmail.com>
2020-02-02 14:41:20 +0530

    header left

--- src/app/components/grid/grid.component.ts
+++ src/app/components/grid/grid.component.ts
@@ -1,3 +1,4 @@
+
 import { Component, OnInit } from '@angular/core';
 import {Grid} from '../../../shared/grid';
 import {gridData} from '../../../shared/grid_data'
@@ -10,8 +11,26 @@ import {gridData} from '../../../shared/grid_data'
 export class GridComponent implements OnInit {
 
   grid_data : Grid[] = gridData
+  data_len= [...Array(this.grid_data.length/3).keys()]
+  grids = this.split(this.grid_data , 3);
 
-  constructor() { }
+  split(arr, subArrSize){
+    let res=[], subArr=[],resIndex=0;
+    for (let index = 0; index < arr.length; index++) {
+      if(resIndex >= subArrSize ) {
+        resIndex =0 
+        res.push(subArr)
+        subArr=[]
+      }
+     subArr.push(arr[index])
+      resIndex++;
+    }
+    res.push(subArr)
+    return res;
+  }
+
+  constructor() {
+   }
 
   ngOnInit() {
   }
