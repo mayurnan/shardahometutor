43a54bc2d6009ca3cb8f1c58323c54a815663d48
mayur-nandu <nandumayur95@gmail.com>
2020-02-10 00:48:29 +0530

    fixing banner images

--- src/shared/carousels.ts
+++ src/shared/carousels.ts
@@ -1,6 +1,8 @@
 import {Carousel} from './carousel'
 import {baseurl} from './BaseURL'
 
+// https://drive.google.com/open?id=1_DtYP7PeY7FH39EYLJVNbWX3Zi5seeA7
+
 export const carousels : Carousel[] = [
     {
         id: '0',
@@ -13,21 +15,25 @@ export const carousels : Carousel[] = [
         id: '0',
         alt:'one',
         title: 'Idea',
-        image: `${baseurl}1_DtYP7PeY7FH39EYLJVNbWX3Zi5seeA7`,
+        // https://drive.google.com/open?id=1aLZrtGGlpHhtAra4jNn5idFVTn9OUIlQ
+        image: `${baseurl}1aLZrtGGlpHhtAra4jNn5idFVTn9OUIlQ`,
+        // image:"",
         description: ''
     },
     {
         id: '1',
         alt:'two',
         title: 'Idea1',
-        image: `${baseurl}1hCKy0s1g-QF_46FpVLUPQm8knQlsLkQn`,
+        // https://drive.google.com/open?id=1EEaYR_mNR7a7XqV39pzbSqMOHWSJesvG//banner1
+        image: `${baseurl}1EEaYR_mNR7a7XqV39pzbSqMOHWSJesvG`,
         description: ''
     },
     {
         id: '2',
         alt:'three',
         title: 'Idea2',
-        image: `${baseurl}1SJlXnf5tzAg17G59hNzSZR0JcibTlsrt`,
+        // https://drive.google.com/open?id=1pgd-AP0NCU7TEz-CJ1ZFvKAAjW-MZZSn //banner2
+        image: `${baseurl}1pgd-AP0NCU7TEz-CJ1ZFvKAAjW-MZZSn`,
         description: ''
     }
 ]
