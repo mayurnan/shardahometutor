eb5065714db6505ea1f5c241a0b4d49754322505
mayur-nandu <nandumayur95@gmail.com>
2020-02-09 21:23:35 +0530

    text resize and image form src

--- src/services/axiosreq.js
+++ src/services/axiosreq.js
@@ -3,10 +3,10 @@ let authcall = require('../../api/axiosCall').authcall
 export default  {
     async  SubmitFrom (data) {
       // console.log(data)
-      return  await authcall.post('http://127.0.0.1:3000/register', data)
+      return  await authcall.post('https://us-central1-shardahometutorbackend.cloudfunctions.net/app/register', data)
     },
     async  SubmitTeacherFrom (data) {
       // console.log(data)
-      return  await authcall.post('http://127.0.0.1:3000/registerAsTeacher', data)
+      return  await authcall.post('https://us-central1-shardahometutorbackend.cloudfunctions.net/app/registerAsTeacher', data)
     }
 }
